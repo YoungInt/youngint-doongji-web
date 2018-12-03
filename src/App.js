e7c020bdef1aa83b425c096c66637c5506a1961b
koo <goosee115@gmail.com>
2018-12-03 15:19:46 +0900

    컴포넌트 이름 변경

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import './App.scss'
-import MapCC from './components/MapCC'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./App.scss";
+import MapCC from "./components/MapPC";
 export default class App extends Component {
   render() {
     return (
       <div>
-        <MapCC />
+        <MapPC />
       </div>
-    )
+    );
   }
 }
--- src/components/MapPC.js
+++ src/components/MapPC.js
@@ -1,37 +1,37 @@
-import React from 'react'
-import * as data from '../API/vending_machine_map.json'
+import React from "react";
+import * as data from "../API/vending_machine_map.json";
 
-export default class MapCC extends React.Component {
+export default class MapPC extends React.Component {
   state = {
     Lat: 33.450701,
     Lng: 126.570667,
     daumMap: null
-  }
+  };
 
   componentDidMount() {
-    this.drawMap()
+    this.drawMap();
   }
   // 지도를 그리는 함수
   drawMap = () => {
-    this.getLocation()
+    this.getLocation();
 
-    const { Lat, Lng } = this.state
-    const mapEl = document.getElementById('map')
+    const { Lat, Lng } = this.state;
+    const mapEl = document.getElementById("map");
     let daumMap = new daum.maps.Map(mapEl, {
       center: new daum.maps.LatLng(Lat, Lng)
-    })
-    this.drawMaker(daumMap)
+    });
+    this.drawMaker(daumMap);
     this.setState({
       daumMap
-    })
-  }
+    });
+  };
   // 마커 생성 함수
   drawMaker = daumMap => {
     data.data.map(position => {
-      var imageSize = new daum.maps.Size(24, 35)
+      var imageSize = new daum.maps.Size(24, 35);
       const imageSrc =
-        'http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/map-marker-icon.png'
-      var markerImage = new daum.maps.MarkerImage(imageSrc, imageSize)
+        "http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/map-marker-icon.png";
+      var markerImage = new daum.maps.MarkerImage(imageSrc, imageSize);
       const marker = new daum.maps.Marker({
         position: new daum.maps.LatLng(
           position.map.latitude,
@@ -40,32 +40,32 @@ export default class MapCC extends React.Component {
         title: position.vending_machine.place,
         image: markerImage,
         map: daumMap
-      })
-    })
-  }
+      });
+    });
+  };
   // 사용자의 좌표값을 가져오는 함수
   getLocation = () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.showPosition)
+      navigator.geolocation.getCurrentPosition(this.showPosition);
     } else {
-      console.log('위치 안잡힘')
+      console.log("위치 안잡힘");
     }
-  }
+  };
   // 현재 위치로 상태 좌표를 업데이트하는 함수
   showPosition = async position => {
     await this.setState({
       Lat: position.coords.latitude,
       Lng: position.coords.longitude
-    })
-    const moveLatLon = new daum.maps.LatLng(this.state.Lat, this.state.Lng)
-    this.state.daumMap.setCenter(moveLatLon)
-  }
+    });
+    const moveLatLon = new daum.maps.LatLng(this.state.Lat, this.state.Lng);
+    this.state.daumMap.setCenter(moveLatLon);
+  };
 
   render() {
     const style = {
-      width: '700px',
-      height: '600px'
-    }
+      width: "700px",
+      height: "600px"
+    };
     return (
       <React.Fragment>
         <div id="map" className="map" style={style} />
@@ -73,6 +73,6 @@ export default class MapCC extends React.Component {
           {this.state.Lat}/{this.state.Lng}
         </div>
       </React.Fragment>
-    )
+    );
   }
 }
