e7d2c0720f65bbabcbd09819daf2a4ebfd22f304
koo <goosee115@gmail.com>
2018-12-11 17:57:36 +0900

    Markup: philosophy 컴포넌트 마크업, 디자인

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,11 +2,13 @@ import React from "react";
 import TeamPC from "../components/TeamPC";
 import MainTop from "../components/MainTop";
 import Footer from "../components/Footer";
+import Philosophy from "../components/Philosophy";
 export default class MainPage extends React.Component {
   render() {
     return (
       <div>
         <MainTop />
+        <Philosophy />
         <TeamPC />
         <Footer />
       </div>
