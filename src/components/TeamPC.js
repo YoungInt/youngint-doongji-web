4fbbae458c55bd9f33a26d3a8e58440cb7b99ba4
koo <goosee115@gmail.com>
2018-12-11 10:58:22 +0900

    Design: 팀 컴포넌트 레이아웃 디자인

--- src/components/TeamPC.js
+++ src/components/TeamPC.js
@@ -1,15 +1,16 @@
 import React from "react";
 import classNames from "classnames";
-import members from "../API/memberAPI";
+import teams from "../API/memberAPI";
 
 export default class TeamPC extends React.Component {
   state = {
-    activeId: 2,
+    members: [...teams, ...teams, ...teams],
+    activeId: 16,
     scrollPositon: 0,
     srcoll: true
   };
   static timer = false;
-
+  async componentDidMount() {}
   throttling = async (e, func) => {
     const deltaX = e.deltaX;
     if (!this.timer) {
@@ -22,11 +23,19 @@ export default class TeamPC extends React.Component {
   };
   handleScroll = deltaX => {
     const direction = deltaX;
-    if (direction > 0 && this.state.activeId < members.length) {
+    if (
+      direction > 0 &&
+      this.state.activeId < this.state.members.length - 3 &&
+      this.state.activeId < this.state.members.length
+    ) {
       this.setState({
         activeId: ++this.state.activeId
       });
-    } else if (direction < 0 && this.state.activeId > 0) {
+    } else if (
+      direction < 0 &&
+      this.state.activeId > 2 &&
+      this.state.activeId > 0
+    ) {
       this.setState({
         activeId: --this.state.activeId
       });
@@ -34,71 +43,71 @@ export default class TeamPC extends React.Component {
   };
 
   render() {
+    const { activeId, members } = this.state;
+
     return (
       <React.Fragment>
-        <div className="team">
-          <div className="team__title">MEET OUR TEAM</div>
-
-          <div className="team__member">
-            <div
-              className="button left"
-              onClick={e =>
-                this.state.activeId > 2 &&
-                this.setState({
-                  activeId: --this.state.activeId
-                })
-              }
-            >
-              뒤로
+        <div className="team-container">
+          <div className="team">
+            <div className="team__title">
+              <div className="title">MEET OUR TEAM</div>
             </div>
-            <div
-              className="carousel-box"
-              id="carousel-box"
-              onWheel={e => this.throttling(e, this.handleScroll)}
-            >
+            <div className="team__member">
               <div
-                className={classNames(
-                  "carousel",
-                  `carousel-${this.state.activeId}`
-                )}
+                className="button left"
+                onClick={e => {
+                  activeId > 2 &&
+                    this.setState({
+                      activeId: activeId - 1
+                    });
+                }}
               >
-                {members.map((member, index) => (
-                  <div
-                    onClick={e => {
-                      this.setState({ activeId: index });
-                      console.log(this.state.activeId, index);
-                    }}
-                    key={index}
-                    className={classNames("item", {
-                      // active: this.state.activeId + 2 === index
-                    })}
-                  >
+                뒤로
+              </div>
+              <div
+                className="carousel-box"
+                id="carousel-box"
+                onWheel={e => this.throttling(e, this.handleScroll)}
+              >
+                <div className={classNames("carousel", `carousel-${activeId}`)}>
+                  {members.map((member, index) => (
                     <div
-                      className={classNames("item__img", {
-                        active: this.state.activeId === index
-                      })}
+                      onClick={e => {
+                        console.log(members.length);
+                        console.log(activeId);
+
+                        index >= 2 &&
+                          index < members.length - 3 &&
+                          this.setState({ activeId: index });
+                      }}
+                      key={index}
+                      className={classNames("item")}
                     >
-                      <img src={member.img} alt="" />
-                      <div>index: {index}</div>
+                      <div
+                        className={classNames("item__img", {
+                          active: activeId === index
+                        })}
+                      >
+                        <img src={member.img} alt="" />
+                      </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
+                </div>
+              </div>
+              <div
+                className="button right"
+                onClick={e =>
+                  activeId !== members.length - 3 &&
+                  this.setState({
+                    activeId: activeId + 1
+                  })
+                }
+              >
+                >앞으로
               </div>
             </div>
-            <div
-              className="button right"
-              onClick={e =>
-                // this.state.activeId !== this.members.length - 3 &&
-                this.setState({
-                  activeId: ++this.state.activeId
-                })
-              }
-            >
-              >앞으로
-            </div>
+            <div className="team__message">{members[activeId].message}</div>
           </div>
-          <div>activeId {this.state.activeId}</div>
-          <div className="team__message" />
         </div>
       </React.Fragment>
     );
