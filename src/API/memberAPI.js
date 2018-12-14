5a95886b81449e6c0071822fbed7381cd2d973b5
koo <goosee115@gmail.com>
2018-12-14 09:10:00 +0900

    Design: 브랜드 디자인 수정

--- src/API/memberAPI.js
+++ src/API/memberAPI.js
@@ -17,15 +17,15 @@ const members = [
     name: "YoungEun, 대표",
     message: "큰 꿈을 꿉시다. 그대로 됩니다."
   },
-  { id: 1, img: img2, name: "김영광", message: "2번 메시지" },
+  { id: 1, img: img2, name: "Y.K.Kim", message: "즐거워" },
   {
     id: 2,
     img: img3,
     name: "JiHee, 경영지원",
     message: "나는야 영인터의 살림꾼"
   },
-  { id: 3, img: img4, name: "박규빈", message: "4번 메시지" },
-  { id: 4, img: img5, name: "김윤배", message: "5번 메시지" },
+  { id: 3, img: img4, name: "규빈님", message: "4번 메시지" },
+  { id: 4, img: img5, name: "윤배님", message: "5번 메시지" },
   {
     id: 5,
     img: img6,
@@ -40,7 +40,7 @@ const members = [
   },
   { id: 7, img: img8, name: "효원님", message: "8번 메시지" },
   { id: 8, img: img9, name: "문규님", message: "9번 메시지" },
-  { id: 9, img: img10, name: "나", message: "10번 메시지" },
+  { id: 9, img: img10, name: "나", message: "아무말아무말" },
   { id: 10, img: img11, name: "승식님", message: "11번 메시지 " }
 ];
 function shuffle(a) {
