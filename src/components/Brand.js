d26e288d75b92472dc979e6ba5ebf9b8a5cdfa27
koo <goosee115@gmail.com>
2018-12-17 17:14:54 +0900

    폴라폴라, 해피 디자인 수정

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -8,41 +8,51 @@ export default class Brand extends React.Component {
     return (
       <React.Fragment>
         <div className="brand-container ">
-          <div className="brand happy">
-            <img className="happy-img" src={happy} alt="" />
-            <div className="wrap happy">
-              <div className="container-title">
-                <h2 className="title">OUR BRAND</h2>
-              </div>
-
-              <div className="content">
-                <div className="title">
-                  <p>해피네임 스티커</p>
-                  <p>HAPPYNAME</p>
+          <div className="brand-wrap">
+            <div className="brand happy" />
+            <div className="brand pola" />
+            <div className="box">
+              <div className="center">
+                <div className="brand-box happy">
+                  <div className="container-title">
+                    <h2 className="title">OUR BRAND</h2>
+                  </div>
+                  <div className="img">
+                    <img className="happy-img" src={happy} alt="" />
+                  </div>
+                  <div className="content">
+                    <div className="title">
+                      <h3 className="bold">해피네임스티커</h3>
+                      <h3>HAPPYNAME</h3>
+                    </div>
+                    <p>
+                      행복한 사진배달부 폴라폴라의 폴라로이드 사진 스티커 자판기
+                    </p>
+                    <Link to="/pola">
+                      <div className="button">자세히보기 ></div>
+                    </Link>
+                  </div>
                 </div>
-                <p>남녀노소 나이불문 쉽게 사용할 수 있는 네임스티커 자판기</p>
+                <div className="brand-box pola">
+                  <div className="container-title" />
+                  <div className="img">
+                    <img className="happy-img" src={pola} alt="" />
+                  </div>
 
-                <Link to="/happy">
-                  <div className="button">자세히 보기 ></div>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="brand pola">
-            <img className="pola-img" src={pola} alt="" />
-            <div className="wrap pola">
-              <div className="container-title" />
-              <div className="content pola">
-                <div className="title">
-                  <p>폴라폴라</p>
-                  <p>POLAPOLAR</p>
+                  <div className="content pola">
+                    <div className="title">
+                      <h3 className="bold">폴라폴라</h3>
+                      <h3>POLAPOLAR</h3>
+                    </div>
+                    <p>
+                      남녀노소 나이불문 쉽게 사용할 수 있는 네임스티커 자판기
+                    </p>
+
+                    <Link to="/happy">
+                      <div className="button">자세히 보기 ></div>
+                    </Link>
+                  </div>
                 </div>
-                <p>
-                  행복한 사진배달부 폴라폴라의 폴라로이드 사진 스티커 자판기
-                </p>
-                <Link to="/pola">
-                  <div className="button">자세히보기 ></div>
-                </Link>
               </div>
             </div>
           </div>
