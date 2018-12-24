7c9eb30c7e79092734b033c3b93fc3ad3d833d29
koo <goosee115@gmail.com>
2018-12-24 11:16:54 +0900

    map 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-import { BrowserRouter, Route } from "react-router-dom";
-import MapPage from "./pages/MapPage";
 ReactDOM.render(
   <App />,
 
