c3737d319e6ac8cdf55f9afee8d96b7215f68503
koo <goosee115@gmail.com>
2018-12-26 11:11:01 +0900

    language provider 추가

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./App.scss";
-
-import {
-  Route,
-  BrowserRouter as Router,
-  Switch,
-  browserHistory
-} from "react-router-dom";
-import MapPage from "./pages/MapPage";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import HappyPage from "./pages/HappyPage";
 import PolaPage from "./pages/PolaPage";
+import { LanguageProvider, LanguageConsumer } from "./context/LanguageContext";
+
 export default class App extends Component {
+  componentDidMount() {}
   render() {
     return (
       <Router>
-        <Switch>
-          <Route exact path="/" component={MainPage} />
-          <Route path="/pola" component={PolaPage} />
-          <Route pah="/happy" component={HappyPage} />
-          {/* <Route path="/map/:id" component={MapPage} />
+        <LanguageProvider>
+          <LanguageConsumer>
+            {value => (
+              <Switch>
+                <Route exact path="/" component={MainPage} />
+                <Route
+                  path="/pola"
+                  component={() => <PolaPage value={value} />}
+                />
+                <Route
+                  pah="/happy"
+                  component={() => <HappyPage value={value} />}
+                />
+                {/* <Route path="/map/:id" component={MapPage} />
           <Route path="/map/" component={MapPage} /> */}
-          {/* <Route path="/map/:id" component={MapPage} />
+                {/* <Route path="/map/:id" component={MapPage} />
           <Route path="/map/" component={MapPage} /> */}
-        </Switch>
+              </Switch>
+            )}
+          </LanguageConsumer>
+        </LanguageProvider>
       </Router>
     );
   }
