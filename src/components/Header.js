3447b1b918b6bde48da3af288ad5a2b26c1e73ae
koo <goosee115@gmail.com>
2018-12-31 14:13:00 +0900

    헤더 스크롤 이벤트 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,48 @@ import React from "react";
 import LangIcon from "../components/LangIcon";
 import homeLogo from "../images/button_home.svg";
 import { Link } from "react-router-dom";
+import classnames from "classnames";
 
 export default class Header extends React.Component {
+  state = {
+    hidden: false
+  };
+  scrollPosition = 0;
+
+  componentDidMount() {
+    this.scrollPosition = window.scrollY;
+    this.scrollEvent = setInterval(() => {
+      window.addEventListener("scroll", this.handleScroll);
+    }, 100);
+  }
+  handleScroll = () => {
+    const currentPosition = window.scrollY;
+    const { hidden } = this.state;
+    this.scrollPosition < currentPosition
+      ? !hidden &&
+        currentPosition > 80 &&
+        this.setState({
+          hidden: true
+        })
+      : hidden &&
+        this.setState({
+          hidden: false
+        });
+    this.scrollPosition = currentPosition;
+  };
+  componentWillUnmount() {
+    clearInterval(this.scrollEvent);
+  }
   render() {
     const { classname, brand } = this.props;
+    const { hidden } = this.state;
     return (
       <header>
-        <div className={`container-wrap ${brand} ${classname}`}>
+        <div
+          className={classnames(`container-wrap ${brand} ${classname}`, {
+            hidden: hidden
+          })}
+        >
           <div className={`container ${brand} ${classname}`}>
             <div className={`header ${classname}`}>
               <div className={`logo ${classname}`}>
