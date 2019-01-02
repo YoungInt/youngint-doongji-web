a85691dda6744ef23fb8c730a9b7d978e494aecd
koo <goosee115@gmail.com>
2019-01-02 18:04:15 +0900

    LangIcon 컴포넌트 오류 수정

--- src/components/LangIcon.js
+++ src/components/LangIcon.js
@@ -5,11 +5,19 @@ import globalIcon from "../images/button_language";
 import bubbleIcon from "../images/common_language_bubble";
 
 export default class LangIcon extends React.Component {
+  handleChangeLanguage = (onChangeLang, lang) => {
+    const changed = lang === "ko" ? "en" : "ko";
+    onChangeLang(changed);
+  };
+
   render() {
     return (
       <LanguageConsumer>
         {({ lang, onChangeLang }) => (
-          <div className={`lang ${this.props.class}`} onClick={onChangeLang}>
+          <div
+            className={`lang ${this.props.class}`}
+            onClick={e => this.handleChangeLanguage(onChangeLang, lang)}
+          >
             <div className="lang__bubble">
               <img className="icon" src={bubbleIcon} alt="" />
               <div className={`text ${this.props.class}`}>
