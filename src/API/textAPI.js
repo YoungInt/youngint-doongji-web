1ece42bbc5367351b59cc1ff0c6d270746d32024
koo <goosee115@gmail.com>
2019-01-04 11:10:51 +0900

    text en 추가

--- src/API/textAPI.js
+++ src/API/textAPI.js
@@ -47,39 +47,38 @@ const mainText = {
     }
   },
   en: {
-    t1: "사람에게 집중한 //n 익숙하지만 '새로운' //n kiosk service",
+    t1: "I'm familiar with people, //n but new //n kiosk service",
     p1:
-      "영인터내셔널은 고객의 아이덴티티를 만들어 주는 //n 키오스크(무인자동화판매기) 개발/유통 회사입니다. //n 고객들이 키오스크를 사용함에 있어 더 나은 사용환경과 //n 경험을 제공하는 것에 집중하고 있습니다.",
-    t2: "세계로 나아가는 //n 글로벌 기업",
+      "Young International is a kiosk (unattended automated vending machine) development / distribution company that creates a customer identity. //n Focusing on providing customers with a better experience and experience in using kiosks. There is.",
+    t2: "global corporation //n moving to the world",
     p2:
-      "영인터내셔널은 해외 고객사와도 함께 합니다. //n 싱가폴, 말레이시아, 인도네시아, 일본 캐나다, //n 대만, 남아프리카공화국 등에 진출해 있습니다.",
-    t3: ["NEW//nVALUE", "GROW //n TOGETHER", "RIGHT //n DIRECTION"],
+      "Young International is also with our overseas clients: //n Singapore, Malaysia, Indonesia, Japan Canada, //n Taiwan, South Africa.",
+    t3: ["NEW //n VALUE", "GROW //n TOGETHER", "RIGHT //n DIRECTION"],
     p3: [
-      "새로운 가치를 //n만듭니다.",
-      "함께 일하고 //n함께 성장합니다.",
-      "올바른 길이라면 //n힘들어도 나아갑니다."
+      "Create a //n new value",
+      "Working with //n grows together.",
+      "If it is the right way, //n it will go even harder."
     ],
-    t4: "우리는 키오스크를 통한 //n *새로운 문화를 탐구*합니다.",
+    t4: "We explore *new cultures* //n through kiosks.",
     p5:
-      "무인자판기 시장은 오래되었지만 그동안 큰 변화가 없었습니다. //n우리는 이 시장에서 통용되는 틀을 넘어, //n고객들의 숨은 니즈를 찾고 솔루션을 찾고자 합니다.//n키오스크의 장점을 '무인'과 '자동화'에만 초점을 두지 않고, //n키오스크를 통한 새로운 문화를 만들어 가고자 합니다.",
+      'The market for unmanned vending machines has been around for a long time,//n but there has not been much change in the past. //n We go beyond the frame of this market, //n seeking the hidden needs of our customers and finding solutions. //n We do not just focus on "unattended" and "automation", //n we want to create a new culture through kiosk.',
     t6:
-      "네임스티커 자판기 개발 *국내최초* //n카카오톡&키오스크 연동 *국내 최초* //n누적 매출 *70억원*//n국내 누적 사용자 *300만명*//n누적 설치대수 *1000대*",
-    p6: "2018년 10월 기준",
-    // 파트너
+      "Development of name sticker vending machine *First in Korea* //n KakaoTalk & Kiosk interlocking *First in Korea* //n Cumulative sales *7 billion* //n Domestic cumulative users *3 million* //n Cumulative installations *1000* ",
+    p6: "As of October 2018",
     p7:
-      "더 좋은 서비스를 제공하고 더 많은 고객들과 만나기 위해 다양한 협력사와 함께 하고 있습니다.",
-    // 브랜드
-    t8: ["해피네임스티커", "폴라폴라"],
+      "We are partnering with a variety of partners to provide better service and meet more customers.",
+    t8: ["Happy Name Sticker", "Pola Polar"],
     p8: [
-      "남녀노소 나이불문 쉽게 사용할 수 있는//n네임스티커 자판기",
-      "행복한 사진배달부 북극곰 폴라폴라의 //n 폴라로이드 사진 스티커 자판기 "
+      "Easy to use, regardless of age, age, and age //n  a Happy name sticker vending machine",
+      "Happy Photo Delivery Polar Polar Polar Polar //n Polaroid Photo Sticker Vending Machine"
     ],
-    button8: "자세히 보기 >",
+    button8: "more",
+
     footer: {
       business:
-        "(주)영인터내셔널 | 사업자 등록번호: 120-88-01280 | 대표:김영은",
+        "Young International Inc. | Business Registration No .: 120-88-01280 |",
       address:
-        "(16967)121, Singal-ro, Giheung-gu, Yongin-si, Gyeonggi-do, Republic of Korea |",
+        "121 Young International (16967) | Shingalou, Giheung-gu, Yongin-si, Gyeonggi-do",
       tel: "070-8856-6377",
       fax: "070-8611-0771"
     },
@@ -125,31 +124,31 @@ const polaText = {
   },
   en: {
     main:
-      "폴라로이드 스티커 배달부,//n*polapolar*입니다.//n소중한 추억을 배달해드려요.",
+      "Polaroid sticker deliverer, //n *PolarPolar*. //n I will deliver your precious memories.",
     use: [
-      "키워드를 입력하여 //n폴라폴라와 연결하고",
-      "채팅창에 바로//n사진을 전송하면",
-      "나만의 폴라로이드 사진을//n간편하게 만들 수 있어요.//n *참 쉽죠?*"
+      "Enter a keyword//n to connect with PolarPolar,",
+      "If you send //n pictures right to the chat window",
+      "You can easily create //n your own Polaroid photo"
     ],
     featureTitle: [
-      "*절취선*이 있어요",
-      "*스티커*입니다.",
-      "*방수*가 되고//n*내구성*이 좋아요",
-      "*팝업(Pop-up)*//n*한정 테마*가 있어요"
+      "*perforation*",
+      "*sticker*",
+      "*waterproof* and *durable*",
+      "*Pop-up* //n *limited theme*"
     ],
     featureDesc: [
-      "폴라폴라는 가운데에 절취선이 있어서//n취향에 따라, 크기에 맞게 사용할 수 있으며//n친구나 연인과 간편하게 나눌 수 있습니다.",
-      "폴라폴라는 가운데에 절취선이 있어서//n취향에 따라, 크기에 맞게 사용할 수 있으며//n친구나 연인과 간편하게 나눌 수 있습니다.",
-      "폴라폴라의 필름은 방수가 되는 재질입니다.//n시간이 지나도 색이 잘 바라거나 찢어지지 않아서//n소중한 사진을 오래 보관할 수 있습니다. ",
-      "일정 기간동안만 만나볼 수 있는 한정 테마를 운영합니다.//n 여러 캐릭터사의 테마와 이벤트 테마를 준비중입니다.//n 다음 팝업테마는 어떤걸까요?기대해주세요!"
+      "Pola Polar has a perforated line in the middle and //n can be used according to your taste, size, and can be easily shared with friends and lovers.",
+      "Pola Polar is even more special because//n you can use it as a sticker.//n Please paste it on your notebook, diary or notebook.",
+      "Polar polar film is a waterproof material. You can keep your precious photos for a long time because //n the color is not well worn or torn off over time.",
+      "We run a theme that can be seen only for a certain period of time . We are preparing various themes and event themes. What is the next pop-up theme?"
     ],
     slideMessage: [
-      "소중한 사진으로 나의 공간을 더욱 의미있게",
-      "특별한 날, 오래도록 남는 선물",
-      "나만의 커스텀 굿즈",
-      "따뜻한 추억들을 모아볼래요"
+      "Making my space more meaningful with precious photos",
+      "Special day, long-lasting gift",
+      "Easily create your own photo book",
+      "Keep your friends, family, companion animals, //n and my best in particular"
     ],
-    message: "볼빨간 배달부,//n폴라폴라를 찾아주세요:-)"
+    message: "Look at the delivery point, //n please find the polar Paula :-)"
   }
 };
 const happyText = {
@@ -189,39 +188,39 @@ const happyText = {
       "해피네임스티커는 더 넓은 세계로 나가 더 많은 고객들은 만나고,//n고객의 소리에 귀 기울여 더 좋은 제품을 개발할 것을 약속드립니다."
   },
   en: {
-    bubble: "나만의 그림은 //n 계속 업데이트 됩니다",
-    main: "누구나 부담없이 만들 수 있는//n*나만의 name sticker* ",
-    cultureTitle: "견출지 그 이상//n*새로운 네임스티커 문화*",
+    bubble: "Your own picture //n will keep updating",
+    main: "Anyone can make it freely //n *my name sticker*",
+    cultureTitle: "Leaves more than //n *new name sticker culture*",
     cultureDesc:
-      "해피네임스티커는 고객의 취향과 필요에 따라 네임스티커로의 용도만이 아닌//n친구와 연인간의 기록의 용도와 재미있는 놀이문화로도 자리 잡았습니다.//n친구와 연인과 네임스티커를 교환하고, 의미있는 곳에 붙여 기록하는 용도로도 사용되고 있습니다.",
+      "Happy name sticker is not only used as a name sticker but also according to customers' taste and needs. //n It is also used as a record of friends and lovers and a fun play culture.//n Friends and lover and name sticker It is also used as an exchange to record in a meaningful place. ",
     culture: [
-      "소중한//n나의 물건에//n표시해요.",
-      "친구와//n 네임스티커//n교환해요",
-      "연인과의//n사랑을//n기록해요"
+      "show //n important //nto my stuff,",
+      "exchange//nname sticker//nwith friends",
+      "record //n my love//n with my lover "
     ],
     versionTitle:
-      "해피네임스티커 자판기는//n*3가지 버전*이 있어//n 상황에 맞는 대응이 가능합니다.",
+      "Happy name sticker vending machines//n have *3 versions* and can respond to n situations",
     version: [
-      "원하는 아이콘을 //n선택할 수 있는//n*해피 ver.*",
-      "눈에 띄는 귀여운//n캐릭터 디자인//n*캐릭터 ver.*",
-      "제휴사 요청에//n맞는 디자인//n*커스텀 ver.*"
+      "You can choose//n your own picture //n *Happy ver.*",
+      "Striking //n cute character design //n *character ver.*",
+      "Affiliate request design to match //n  *custom ver.*"
     ],
 
     featureTitle: [
-      "*글꼴 선택이 가능*합니다.",
-      "고객이 인쇄하고 싶은//n*모티브를 선택*할 수 있습니다.",
-      "*우수한 스티커 품질*",
-      "*다양한 형태의 디자인 시도*"
+      "*Can select fonts*",
+      "*You can* *select*//n your own picture.",
+      "*Excellent sticker quality*",
+      "*Various design attempts*"
     ],
     featureDesc: [
-      "여러번의 테스트를 거쳐 사용자가 글꼴을 편집 할 수 있도록 적용했습니다.//n해피버전 UI내에서 네가지 글꼴 중 원하는 글꼴로 선택이 가능합니다.//n소비자와 제휴사의 의견을 적극 반영하여 업데이트 적용한 부분입니다.",
-      "고객이 직접 원하는 모티브 디자인을 선택할 수 있는 기능으로 //n타사와 가장 대비되는 해피네임스티커의 강점입니다. //n모티브는 주기적으로 새로운 디자인으로 업데이트되고 있으며, //n제휴를 맺은 캐릭터사의 캐릭터 디자인도 개발하여 출시하고 있습니다.",
-      "해피네임스티커의 인쇄 부분은 여러번 지워도 잘 지워지지 않습니다. //n또한 최고급 품질의 라미네이팅 코팅으로 내구성이 매우 우수하며,//n열에 견디는 정도와 접착력 또한 여러번의 테스트를 통해 검증되었습니다.",
-      "해피네임스티커는 디즈니코리아, 산리오코리아, 아이코닉스, 핑크퐁 등 //n여러 캐릭터사의 캐릭터를 적용한 디자인을 출시하고 있으며,//n네임스티커가 더 폭넓은 용도로 사용될 수 있도록.//n새롭고 다양한 칼선을 적용한 스티커를 개발하고 있습니다."
+      "After several tests, we have made it possible for users to edit fonts. //n You can choose any of the four fonts in the Happy Version UI. Part. ",
+      "The ability to choose the motif design that you want yourself is //n The strength of Happy Name Sticker which contrasts most with other companies. //n The motif is updated periodically with new design, We also developed and released a character design. ",
+      "The print portion of the Happy Name sticker is not erased even if it is erased many times. //n The laminating coating is also of the highest quality and has excellent durability. //n Resistance to n heat and adhesion has also been verified by many tests.",
+      "Happy name stickers are designed with characters from Disney Korea, Sanrio Korea, Ikonix, Pink Pong, and many other characters. //n The name sticker can be used for a wider range of applications.//n We are developing a new variety of cutlery stickers. "
     ],
-    messageTitle: "*더 좋은 제품으로*//n*더 넓은 곳으로*",
+    messageTitle: "*Better product* //n *to a wider place*",
     messageDesc:
-      "해피네임스티커는 더 넓은 세계로 나가 더 많은 고객들은 만나고,//n고객의 소리에 귀 기울여 더 좋은 제품을 개발할 것을 약속드립니다."
+      "Happy Name Sticker promises to go to a wider world, meet more customers, and //n listen to customers to develop better products."
   }
 };
 
